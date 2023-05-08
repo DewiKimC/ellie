8b66214b5ce90c526ab160f4fdab38e59a1da201
DewiKimC <97511825+DewiKimC@users.noreply.github.com>
2023-05-08 16:43:21 +0200

    Cirkle in svg and div, made an array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const data = [1000, 2000, 3000, 4000, 5000];
+const customWidth = 200;
+
+//ik maak de cicle waarbij ik de eigenschappen in een aparte constanten zodat deze overzichtelijk zijn en makkelijk aan te passen zijn. 
+const Circle = (props) => {
+  const svgWidth = 1000;
+  const svgHeight = 1000;
+  const circleCenterX = svgWidth / 2;
+  const circleCenterY = svgHeight / 2;
+  const svgFill = "purple"
+
+  return (
+    <svg width={svgWidth} height={svgHeight}>
+      <circle
+        cx={circleCenterX}
+        cy={circleCenterY}
+        r={props.radius}
+        fill={svgFill}
+      />
+    </svg>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Circle radius={customWidth}/>
+      <div style={{backgroundColor: "#ff43e0", width: '50px', height: '50px', borderRadius: '50%'}}></div>
     </div>
   );
 }
